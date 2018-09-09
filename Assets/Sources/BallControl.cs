6ef3bc83a4cfaf43c6b47118caf2ff794fe1e1dd
burak.iren <burakiren54@gmail.com>
2018-09-09 23:47:25 +0300

    collecting coins and scoring

--- Assets/Sources/BallControl.cs
+++ Assets/Sources/BallControl.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallControl : MonoBehaviour {
 
     Rigidbody physic;
     public int speed;
+    int scoreCounter= 0;
+    public int numberOfCollectedCoins;
+    public Text scoreText;
+    public Text gameFinishText;
 
 	void Start () {
 
@@ -22,5 +27,22 @@ public class BallControl : MonoBehaviour {
 
         physic.AddForce(vector3*speed);
 
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Coin"){
+
+            other.gameObject.SetActive(false);
+            scoreCounter++;
+            scoreText.text = "Score : " + scoreCounter;
+            //Destroy(other.gameObject);
+
+            if(scoreCounter == numberOfCollectedCoins){
+                gameFinishText.text = "OYUN BİTTİ";
+            }
+        }
+	
 	}
 }
