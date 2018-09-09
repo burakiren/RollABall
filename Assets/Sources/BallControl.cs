034f1364fec5a362e1350dd7a8772960b3988796
burak.iren <burakiren54@gmail.com>
2018-09-09 22:22:01 +0300

    movement camera with ball

---
+++ Assets/Sources/BallControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    Rigidbody physic;
+    public int speed;
+
+	void Start () {
+
+        physic = GetComponent<Rigidbody>();
+		
+	}
+	
+	void FixedUpdate () {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vector3 = new Vector3(horizontal, 0 , vertical);
+
+        physic.AddForce(vector3*speed);
+
+	}
+}
---
+++ Assets/Sources/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject ball;
+    public Vector3 distanceBetweenBallAndCamera;
+
+	void Start () {
+        distanceBetweenBallAndCamera = transform.position - ball.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = ball.transform.position + distanceBetweenBallAndCamera;
+	}
+}
